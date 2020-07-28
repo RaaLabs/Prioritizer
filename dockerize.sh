6badaa5415337ca7351436c72dbdc90829539f4f
srudaa <sigbjorn.rudaa@raalabs.com>
2020-07-28 16:29:06 +0200

    test

--- dockerize.sh
+++ dockerize.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 export VERSION=$(git tag --sort=-version:refname | head -1)
 docker build --no-cache -f ./Source/Dockerfile -t shipos/timeseries-prioritizer:$VERSION .
 docker push shipos/timeseries-prioritizer:$VERSION
